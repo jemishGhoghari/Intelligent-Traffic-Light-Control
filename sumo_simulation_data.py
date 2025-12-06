5e66d4ae1dddfef8dd9e86492d44e1b0eeb90feb
Jemish Ghoghari <jemishghoghari50@gmail.com>
2025-12-06 19:56:37 +0100

    implement lane feature and phase feature extraction

--- sumo_simulation_data.py
+++ sumo_simulation_data.py
@@ -1,3 +1,6 @@
+import timeit
+import random
+import numpy as np
 import os
 import sys
 
@@ -14,25 +17,155 @@ elif sys.platform == "win32":
 else:
     import libsumo
 
-import numpy as np
-import random
-import timeit
 
 sumoBinary = "sumo-gui"  # or "sumo-gui" for graphical version
 sumoConfig = "./sumo_ingolstadt/simulation/24h_sim.sumocfg"
 sumoCmd = [sumoBinary, "-c", sumoConfig]
 
 
+class SUMOSimulation:
+    def __init__(self):
+        self.queue_length = 0
+        self.waiting_time = 0
+        self.num_vehicles = 0
+        self.mean_speed = 0
+        self.edge_id = 0
+        self.t_actual = 0
+        self.t_expected = 0
+        self.delay = 0
+
+    def _get_lane_features(self, lane_id: int):
+        """
+        Get Lane features like Queue Length, Waiting time of the vehicle, Number of Vehicle on the Lane,
+        Mean speed of the vehcles, and delay.
+
+        Args:
+            lane_id (int): lane id of the incoming or outgoing edge
+        """
+
+        # Get total queue length at the stopline of specific lane
+        self.queue_length = self._get_queue_length_near_stopline(
+            lane_id, distance_from_stop=50
+        )
+
+        # Calculate Vehicle Waiting times and Number of Vehicles.
+        self.waiting_time = traci.lane.getWaitingTime(lane_id)
+        self.num_vehicles = traci.lane.getLastStepVehicleNumber(lane_id)
+        self.mean_speed = traci.lane.getLastStepMeanSpeed(lane_id)
+
+        # Delay: Calculate the delay -> realistic travel time - expected travel time
+        self.edge_id = traci.lane.getEdgeID(lane_id)
+        self.t_actual = traci.edge.getTraveltime(self.edge_id)
+        lane_length = traci.lane.getLength(lane_id)
+        v_max = traci.lane.getMaxSpeed(lane_id)
+        self.t_expected = lane_length / v_max if v_max > 0 else 0.0
+        self.delay = max(0.0, self.t_actual - self.t_expected)  # [s]
+
+        return np.array(
+            [
+                self.queue_length,
+                self.waiting_time,
+                self.num_vehicles,
+                self.mean_speed,
+                self.delay,
+            ],
+            dtype=float,
+        )
+
+    def _get_queue_length_near_stopline(self, lane_id: int, distance_from_stop: float):
+        """_summary_
+
+        Args:
+            lane_id (int): lane id of incoming or outgoing link
+            distance_from_stop (float): queue length within given distance from stopline
+        """
+
+        lane_length = traci.lane.getLength(lane_id)
+        vehicle_ids = traci.lane.getLastStepVehicleIDs(lane_id)
+
+        q = 0
+
+        for v_id in vehicle_ids:
+            pos = traci.vehicle.getLanePosition(v_id)
+            speed = traci.vehicle.getSpeed(v_id)
+
+            if lane_length - pos <= distance_from_stop and speed < 0.1:
+                q += 1
+
+        return q
+
+    def _get_num_phases(self, tls_id: str):
+        current_prog = traci.trafficlight.getProgram(tls_id)
+
+        for logic in traci.trafficlight.getCompleteRedYellowGreenDefinition(tls_id):
+            if logic.programID == current_prog:
+                return len(logic.phases)
+
+        # fallback
+        return len(
+            traci.trafficlight.getCompleteRedYellowGreenDefinition(tls_id)[0].phases
+        )
+
+    def _get_state(self, tls_id: str):
+        """Extract State of the Intersection including Lane features and Phase features
+
+        Args:
+            tls_id (string): Tls ID of the specific Intersection
+        """
+
+        # Extract lane features
+        lanes = sorted(set(traci.trafficlight.getControlledLanes(tls_id)))
+        lane_features = [self._get_lane_features(l) for l in lanes]
+        lane_features = np.concatenate(
+            lane_features, axis=0
+        )  # shape = 5 * n_lanes (n_lanes = 22 for Ingolstadt Saturn Arena Intersection)
+
+        # Extract phase features
+        num_phases = self._get_num_phases(tls_id)
+
+        phase_idx = traci.trafficlight.getPhase(tls_id)
+        phase_one_hot = np.zeros(num_phases, dtype=float)
+        phase_one_hot[phase_idx] = 1.0
+
+        phase_total = traci.trafficlight.getPhaseDuration(tls_id)
+        phase_spent = traci.trafficlight.getSpentDuration(tls_id)
+        sim_time = traci.simulation.getTime()
+        phase_remain = max(
+            0.0, traci.trafficlight.getNextSwitch(tls_id)
+        )  # get exact time at which transition is scheduled
+
+        # Normalize times to something [0, 1] to Help Neural Network
+        max_phase_time = max(1.0, phase_total)
+        phase_spent_norm = phase_spent / max_phase_time
+        phase_remain_norm = phase_remain / max_phase_time
+
+        phase_features = np.concatenate(
+            [
+                phase_one_hot,
+                np.array([phase_spent_norm, phase_remain_norm], dtype=float),
+            ]
+        )
+
+        state = np.concatenate([lane_features, phase_features])
+        return state
+
+
 def main():
     traci.start(sumoCmd)
     step = 0
+
+    env = SUMOSimulation()
+
+    tls_id = "7628244053"
+
     try:
         while True:  # Run for 1000 simulation steps
             traci.simulationStep()
 
-            vehicle_ids = traci.vehicle.getIDList()
+            state = env._get_state(tls_id)
             print(
-                f"Step {step}: Number of vehicles in the simulation: {len(vehicle_ids)}"
+                # f"Step {step}: Number of vehicles in the simulation: {len(vehicle_ids)}"
+                state
             )
             step += 1
     except KeyboardInterrupt:
@@ -42,92 +175,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-##################################################################################################
-##########:= SUMO Simulation setup for the traffic light state and traffic simulation =:##########
-##################################################################################################
-# class SumoSimulationData:
-#     def __init__(
-#         self,
-#         Model,
-#         Memory,
-#         sumoCmd,
-#         gamma,
-#         max_steps,
-#         green_duration,
-#         yellow_duration,
-#         num_states,
-#         num_actions,
-#         training_epochs,
-#     ):
-#         self._Model = Model
-#         self._Memory = Memory
-#         self._sumoCmd = sumoCmd
-#         self._gamma = gamma
-#         self._step = 0
-#         self._max_steps = max_steps
-#         self._green_duration = green_duration
-#         self._yellow_duration = yellow_duration
-#         self._num_states = num_states
-#         self._num_actions = num_actions
-#         self._training_epochs = training_epochs
-#         self._reward_store = []
-#         self._cumulative_wait_store = []
-#         self._queue_length_store = []
-
-#     def run_simulation(self, episode, epsilon):
-#         start_time = timeit.default_timer()
-#         traci.start(self._sumoCmd)
-
-#         print("Start simulation...")
-
-#         self._step = 0
-#         self._waiting_times = {}
-#         self._sum_waiting_time = 0
-#         self._sum_negative_reward = 0
-#         self._sum_queue_length = 0
-#         old_total_wait = 0
-#         old_state = -1
-#         old_action = -1
-
-#         while self._step < self._max_steps:
-#             # current_state = self.
-#             traci.simulationStep()
-#             self._get_state()
-#             self._step += 1
-
-#     def _get_state(self):
-#         state = np.zeros(self._num_states)
-#         car_list = traci.vehicle.getIDList()
-
-#         for car_id in car_list:
-#             lane_pos = traci.vehicle.getLanePosition(car_id)
-#             lane_id = traci.vehicle.getLaneID(car_id)
-#             lane_pos = 750 - lane_pos  # Invert position to measure from traffic light
-#             # print(f"Car ID: {car_id}, Lane ID: {lane_id}, Lane Position: {lane_pos}")
-
-# if __name__ == "__main__":
-#     # Example usage
-#     sumoBinary = "sumo"  # or "sumo-gui" for graphical version
-#     sumoConfig = "./sumo_ingolstadt/simulation/24h_sim.sumocfg"
-#     sumoCmd = [sumoBinary, "-c", sumoConfig]
-
-#     simulation = SumoSimulationData(
-#         Model=None,
-#         Memory=None,
-#         sumoCmd=sumoCmd,
-#         gamma=0.9,
-#         max_steps=1000,
-#         green_duration=30,
-#         yellow_duration=5,
-#         num_states=10,
-#         num_actions=4,
-#         training_epochs=10,
-#     )
-
-#     try:
-#         simulation.run_simulation(episode=1, epsilon=0.1)
-#     except KeyboardInterrupt:
-#         traci.close()
-#         sys.exit()
